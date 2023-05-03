65f4a7883077eb8cdd82a6ca7155c396c6844ced
obiwan-gold <individualog@gmail.com>
2023-05-03 19:13:18 +0100

    Created Task model

--- src/main/java/com/obinstodo/model/Task.java
+++ src/main/java/com/obinstodo/model/Task.java
@@ -1,4 +1,14 @@
 package com.obinstodo.model;
 
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
 public class Task {
+    private int id;
+    private String title;
+    private String description;
+    private Date dueDate;
+    private boolean completed;
 }
