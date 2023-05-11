a571edae275ce28d92a35fce2393c3fa9aee40ef
obiwan-gold <individualog@gmail.com>
2023-05-11 14:10:20 +0100

    Created TaskRepository

--- src/main/java/com/obinstodo/controller/TaskController.java
+++ src/main/java/com/obinstodo/controller/TaskController.java
@@ -3,9 +3,11 @@ package com.obinstodo.controller;
 import com.obinstodo.model.Task;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
+import java.util.List;
 
 @RestController
 public class TaskController {
@@ -13,6 +15,14 @@ public class TaskController {
     @Getter
     @Setter
     private Task task;
+
+    @GetMapping("/tasks")
+    public List<Task> getAllTasks() {
+        // Retrieve the tasks from the data source or repository
+
+        // Return the tasks as the response with an appropriate HTTP status code
+        return ResponseEntity.ok(tasks);
+    }
 
     public void createTask(String title, String description, Date dueDate, boolean completed) {
         Task task = Task.builder()
