48ce50b672ff3820921c6d6a4d35fa74ab64051a
Obin Gold <individualog@gmail.com>
2023-05-12 20:47:24 +0100

    Moved initializTasks() from Repository layer to Service layer (#9)
    * Added all necessary annotations & components for GET method endpoint
    
    * Moved initializTasks() from Repository layer to Service layer

--- src/main/java/com/obinstodo/todolist/repository/InMemoryTaskRepository.java
+++ src/main/java/com/obinstodo/todolist/repository/InMemoryTaskRepository.java
@@ -17,27 +17,23 @@ public class InMemoryTaskRepository implements TaskRepository {
     }
 
     public void initializeTasks() {
-        Task task1 = new Task(1, "Description 1");
-        Task task2 = new Task(2, "Description 2");
-        Task task3 = new Task(3, "Description 3");
 
-        tasks.add(task1);
-        tasks.add(task2);
-        tasks.add(task3);
-    }
-    @Override
-    public void addTask(String task) {
-
+        // Initialize tasks
     }
 
     @Override
-    public void updateTask(String task) {
-
+    public void addTask(Task task) {
+        // Implement the logic to add a task to the repository
     }
 
     @Override
-    public void deleteTask(String task) {
+    public void updateTask(Task task) {
+        // Implement the logic to update a task in the repository
+    }
 
+    @Override
+    public void deleteTask(String taskId) {
+        // Implement the logic to delete a task from the repository
     }
 
     @Override
--- src/main/java/com/obinstodo/todolist/service/DefaultTaskService.java
+++ src/main/java/com/obinstodo/todolist/service/DefaultTaskService.java
@@ -8,28 +8,43 @@ import java.util.List;
 
 @Service
 public class DefaultTaskService implements TaskService {
+  
+    private final List<Task> tasks;
+    private final TaskRepository taskRepository;
 
-    public DefaultTaskService(TaskRepository taskRepository) {
-        taskRepository.initializeTasks();
+    public DefaultTaskService(List<Task> tasks, TaskRepository taskRepository) {
+        this.tasks = tasks;
+        this.taskRepository = taskRepository;
+        initializeTasks();
+    }
+
+    private void initializeTasks() {
+        Task task1 = new Task(1, "Description 1");
+        Task task2 = new Task(2, "Description 2");
+        Task task3 = new Task(3, "Description 3");
+
+        tasks.add(task1);
+        tasks.add(task2);
+        tasks.add(task3);
     }
 
     @Override
     public List<Task> getAllTasks() {
-        return null;
+        return taskRepository.getAllTasks();
     }
 
     @Override
     public void addTask(Task task) {
-
+      taskRepository.addTask(task);
     }
 
     @Override
     public void updateTask(Task task) {
-
+        taskRepository.updateTask(task);
     }
 
     @Override
     public void deleteTask(String taskId) {
-
+        taskRepository.deleteTask(taskId);
     }
 }
