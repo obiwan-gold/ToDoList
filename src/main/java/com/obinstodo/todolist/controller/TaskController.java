387fc344ec3ad10cff93e7376f6689b4e2e3f2ad
Obin Gold <individualog@gmail.com>
2023-05-18 16:39:24 +0100

    Created POST method (#31)
    Co-authored-by: obingurung <obingurung@hollandandbarrett.com>

--- src/main/java/com/obinstodo/todolist/controller/TaskController.java
+++ src/main/java/com/obinstodo/todolist/controller/TaskController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -61,6 +62,16 @@ public class TaskController {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Task not updated");
         }
         return ResponseEntity.ok("Task updated successfully!");
+    }
+
+    @PostMapping("/tasks")
+    public ResponseEntity<String> createTask(@RequestBody Task newTask) {
+        boolean isCreated = taskService.createTask(newTask);
+
+        if (!isCreated){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Task not created");
+        }
+        return ResponseEntity.ok("Task created successfully!");
     }
 
     @DeleteMapping("/tasks/{taskId}")
