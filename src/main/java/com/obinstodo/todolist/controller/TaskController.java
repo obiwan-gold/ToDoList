fb586d40501601ddd8d248f61a11e916e09212b5
obiwan-gold <individualog@gmail.com>
2023-05-25 17:58:05 +0100

    Set up github actions(ci.yml) & deployment.yaml for k8s

--- src/main/java/com/obinstodo/todolist/controller/TaskController.java
+++ src/main/java/com/obinstodo/todolist/controller/TaskController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/todo")
+@RequestMapping("/api")
 public class TaskController {
 
     private final TaskService taskService;
